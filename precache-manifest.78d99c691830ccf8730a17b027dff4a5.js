d585fee19a2eb229ad7c2495f71f7e47af0a67a6
Harry Davis <harrytobydavis@gmail.com>
2020-07-30 20:49:34 +0100

    Updates

---
+++ precache-manifest.78d99c691830ccf8730a17b027dff4a5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c101287a66695bd5a7ee9458911d407",
+    "url": "/code4all/index.html"
+  },
+  {
+    "revision": "b78224cbf28afb80c43f",
+    "url": "/code4all/static/css/main.58858b3b.chunk.css"
+  },
+  {
+    "revision": "fa7d713867fc95ca76be",
+    "url": "/code4all/static/js/2.f707f4c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/code4all/static/js/2.f707f4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78224cbf28afb80c43f",
+    "url": "/code4all/static/js/main.1b2d97ab.chunk.js"
+  },
+  {
+    "revision": "0331a941f1f6f9a61afe",
+    "url": "/code4all/static/js/runtime-main.1bf11e15.js"
+  },
+  {
+    "revision": "e5f719bda9f76a9fb304acdacabe92aa",
+    "url": "/code4all/static/media/black-laptop.e5f719bd.jpg"
+  },
+  {
+    "revision": "117af26773fcabe3b72ba7dce4be2d35",
+    "url": "/code4all/static/media/dark-laptop.117af267.jpg"
+  },
+  {
+    "revision": "1f4ea50363cd710b08880e54b7ccc2ed",
+    "url": "/code4all/static/media/home.1f4ea503.svg"
+  },
+  {
+    "revision": "5f49ffe69ebaad68eecc8dee3d0fc8f9",
+    "url": "/code4all/static/media/phone-laptop.5f49ffe6.jpg"
+  },
+  {
+    "revision": "93f5ba64d5e2c93a873360e1dd80c111",
+    "url": "/code4all/static/media/python.93f5ba64.svg"
+  },
+  {
+    "revision": "b1df5a8ea82250fd39468fdee350c541",
+    "url": "/code4all/static/media/stats-laptop.b1df5a8e.jpg"
+  },
+  {
+    "revision": "2f5697ace0fbb0cd995cd0256d8f8b69",
+    "url": "/code4all/static/media/tea-laptop.2f5697ac.jpg"
+  },
+  {
+    "revision": "67c98214775557f8e470e47572309bde",
+    "url": "/code4all/static/media/web-dev0.67c98214.svg"
+  }
+]);
