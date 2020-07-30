83f6e059703f66785d10d24a83fe7f2915003258
Harry Davis <harrytobydavis@gmail.com>
2020-07-30 21:08:08 +0100

    Added link to my github & modified aims css

--- src/components/sections/about-us.js
+++ src/components/sections/about-us.js
@@ -1,6 +1,9 @@
 //React dependencies
 import React from 'react';
 
+//SVGs
+import { ReactComponent as GithubIcon } from './../../assets/svgs/github-icon.svg';
+
 //Styling
 import './sections.scss';
 
@@ -17,6 +20,8 @@ const AboutUs = () => {
         <em>A project created in the aftermath the GSL Summer Catalyst</em>
 
         <p>Being developed by GCSE student Harry D.</p>
+
+        <a href="https://github.com/harryd05" target="_blank" rel="noopener noreferrer"><GithubIcon alt="github icon" /></a> <br />
 
         <button onClick={() => alert('not implemented yet...')}>Submit a question</button>
       </div>
--- src/components/sections/aims.js
+++ src/components/sections/aims.js
@@ -10,6 +10,7 @@ const Aims = () => {
       <h1>Our aims</h1>
       <ul>
         <li><b>everyone</b> should have the opportunity to <em>learn to code</em>, irrespective of money & background</li>
+        <hr />
         <li><u>improve diversity & representation</u> within the coding community, atleast 80% of programmers are men - <b>we need to change this</b></li>
       </ul>
     </div>
