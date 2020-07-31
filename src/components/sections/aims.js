f100082b0b24a2dc0faf232c5c68e1326d922eb5
Harry Davis <harrytobydavis@gmail.com>
2020-07-31 10:39:41 +0100

    Added link to SDGs to aims

--- src/components/sections/aims.js
+++ src/components/sections/aims.js
@@ -12,6 +12,8 @@ const Aims = () => {
         <li><b>everyone</b> should have the opportunity to <em>learn to code</em>, irrespective of money & background</li>
         <hr />
         <li><u>improve diversity & representation</u> within the coding community, atleast 80% of programmers are men - <b>we need to change this</b></li>
+        <hr />
+        <li>These link to the <em>quality education</em> & <u>gender equality</u> <a href="https://sdgs.un.org/goals" target="_blank" rel="noopener noreferrer">Sustainable Development Goals</a></li>
       </ul>
     </div>
   );
